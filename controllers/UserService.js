db1cda2930e2013c4898421647155eb0ad2db03f
Nelson Rios <nelsonmatias9@hotmail.com>
2016-01-11 12:57:08 -0300

    Estructura inicial de web services para usuarios

---
+++ controllers/UserService.js
@@ -0,0 +1,100 @@
+'use strict';
+
+exports.usersGet = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+   **/
+
+var examples = {};
+  
+  examples['application/json'] = [ {
+  "deleted" : true,
+  "name" : "aeiou",
+  "comment" : "aeiou",
+  "links" : [ {
+    "rel" : "aeiou",
+    "href" : "aeiou"
+  } ],
+  "id" : "aeiou"
+} ];
+  
+
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+exports.usersPost = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+   * user (NewUser)
+   **/
+
+var examples = {};
+  
+
+  
+  res.end();
+}
+exports.usersIdGet = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+   * id (String)
+   **/
+
+var examples = {};
+  
+  examples['application/json'] = {
+  "deleted" : true,
+  "name" : "aeiou",
+  "comment" : "aeiou",
+  "links" : [ {
+    "rel" : "aeiou",
+    "href" : "aeiou"
+  } ],
+  "id" : "aeiou"
+};
+  
+
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+exports.usersIdPut = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+   * id (String)
+   * user (UpdateUser)
+   **/
+
+var examples = {};
+  
+
+  
+  res.end();
+}
+exports.usersIdDelete = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+   * id (String)
+   **/
+
+var examples = {};
+  
+
+  
+  res.end();
+}
